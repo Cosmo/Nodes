1c7d605b06f5c8ff0065fddcfbc6e614ba72dae3
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-10-31 13:19:12 +0100

    Add basic node class

--- Sources/Nodes/Nodes.swift
+++ Sources/Nodes/Nodes.swift
@@ -1,3 +1,15 @@
-struct Nodes {
-    var text = "Hello, World!"
+public class Node<T: Hashable> {
+    public var value: T
+    public weak var parent: Node?
+    public var children = [Node]()
+    
+    public init(value: T) {
+        self.value = value
+    }
+    
+    /// Adds a sub-node.
+    public func addChild(node: Node) {
+        children.append(node)
+        node.parent = self
+    }
 }
--- Tests/NodesTests/NodesTests.swift
+++ Tests/NodesTests/NodesTests.swift
@@ -3,10 +3,7 @@ import XCTest
 
 final class NodesTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Nodes().text, "Hello, World!")
+        
     }
 
     static var allTests = [
