eb849fe4f11bea1408502fc5493eb3f38d4a21d8
Devran "Cosmo" Uenal <maccosmo@gmail.com>
2019-10-31 13:23:32 +0100

    Add multiple useful properties for a tree

--- Sources/Nodes/Nodes.swift
+++ Sources/Nodes/Nodes.swift
@@ -37,4 +37,69 @@ public class Node<T: Hashable> {
     public var ancestorsIncludingSelf: [Node] {
         return ancestors + [self]
     }
+    
+    /// A Boolean value indicating whether the node is without children.
+    public var isLeaf: Bool {
+        return children.isEmpty
+    }
+    
+    /// Returns all nodes with no children.
+    public var leaves: [Node] {
+        return children.filter { $0.isLeaf }
+    }
+    
+    /// Returns the number of leaves.
+    public var breadth: Int {
+        return leaves.count
+    }
+    
+    /// A Boolean value indicating whether the node has children.
+    public var isBranch: Bool {
+        return !children.isEmpty
+    }
+    
+    /// Returns all nodes with at least one child.
+    public var branches: [Node] {
+        return children.filter { $0.isBranch }
+    }
+    
+    /// Returns the distance between a node and the root.
+    public var depth: Int {
+        return ancestors.count
+    }
+    
+    /// The number of edges between the current node and the root.
+    public var level: Int {
+        return depth + 1
+    }
+    
+    /// A Boolean value indicating whether the current node is the top node.
+    public var isRoot: Bool {
+        return parent == nil
+    }
+    
+    /// Returns the top node.
+    public var root: Node {
+        guard let rootNode = ancestorsIncludingSelf.first else {
+            fatalError("Root element not found.")
+        }
+        return rootNode
+    }
+    
+    /// Returns the number of children.
+    public var degree: Int {
+        return children.count
+    }
+    
+    /// Returns all children, moving down from the current node to all leaves.
+    public var descendants: [Node] {
+        var nodes = [Node]()
+        if isBranch {
+            nodes.append(contentsOf: children)
+            for child in children {
+                nodes.append(contentsOf: child.descendants)
+            }
+        }
+        return nodes
+    }
 }
